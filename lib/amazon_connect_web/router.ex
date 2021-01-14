a19d5c1483bc2987a31c2e224536b1ab85338d54
Jorge Acosta Lemus <jorge@makingdevs.com>
2021-01-14 10:34:27 -0600

    Agregando route del resource de user y ruta para api de user

--- lib/amazon_connect_web/router.ex
+++ lib/amazon_connect_web/router.ex
@@ -17,20 +17,10 @@ defmodule AmazonConnectWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/users/api/:number_client", UserController, :get
+    resources "/users", UserController
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", AmazonConnectWeb do
-  #   pipe_through :api
-  # end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
   if Mix.env() in [:dev, :test] do
     import Phoenix.LiveDashboard.Router
 
